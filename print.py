97f0beeec9b3be244342b85bb1e4ffc558f25fc0
saurabh sharma <saurabhsharma.yash2004@gmail.com>
2025-09-06 15:29:49 +0530

    day 1

--- print.py
+++ print.py
@@ -5,8 +5,8 @@ print("apple" "lotus"  "red")
 print("apple", "lotus" , "red") #  (,) separate them
 print("1+2")  # if we write anything inside Quotation mark it will read as string
 print(1 + 2)
-print("Line1\nLine2\nTabbed") # if we use /n it will start from new line
-print("Line1\tLine2\tTabbed") # if we use /t it will give space in b/w letters
+print("Line1\nLine2\nTabbed") # if we use \n it will start from new line
+print("Line1\tLine2\tTabbed") # if we use \t it will give space in b/w letters
 print("A", "B", "C")
 print("A", "B", "C", sep="-")
 print("A", "B", "C", sep="---", end="END")
