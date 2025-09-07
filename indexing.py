8429a5057884521f8a6bff226ba453732befce1b
saurabh sharma <saurabhsharma.yash2004@gmail.com>
2025-09-07 22:17:02 +0530

    day2

---
+++ indexing.py
@@ -0,0 +1,25 @@
+#Positive Indexing
+
+text = "Python"
+print(text[0])  
+print(text[3])  
+
+#Negative Indexing
+
+text = "Python"
+print(text[-1])  
+print(text[-3])  
+
+n = "saurabh"
+print(n[3])
+print(n[-3])
+
+#ques
+
+word = "DataScience"
+print(text[0])  
+print(text[-1]) 
+print(text[4])  
+print(text[-3]) 
+print(text[0] + text[-1])  
+print(text[1] + text[-2])  
