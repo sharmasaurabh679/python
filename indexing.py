3020943e0d118a4490075601f147d39f2b5047bd
saurabh sharma <saurabhsharma.yash2004@gmail.com>
2025-09-07 22:33:02 +0530

    day2

--- indexing.py
+++ indexing.py
@@ -21,5 +21,40 @@ print(text[0])
 print(text[-1]) 
 print(text[4])  
 print(text[-3]) 
-print(text[0] + text[-1])  
-print(text[1] + text[-2])  
+
+
+my_list = [10, 20, 30, 40, 50]
+print(my_list[2])
+
+my_list = [1, 2, 3, 4, 5]
+print(my_list[-1])
+
+my_list = [3, 6, 9, 12, 15]
+print(my_list[-3])
+
+my_list = ['apple', 'banana', 'cherry', 'date']
+print(my_list[0]) # output: 'apple'
+print(my_list[1]) 
+
+animals = ['cat', 'dog', 'elephant', 'tiger']
+print(animals[2])  
+print(animals[-1]) 
+
+numbers = [10, 20, 30, 40, 50]
+print(numbers[4])  
+print(numbers[0])  
+print(numbers[-3])  
+
+cars = ['BMW', 'Audi', 'Tesla', 'Toyota']
+print(cars[1])  
+print(cars[-4])  
+
+cities = ['London', 'Paris', 'Tokyo', 'New York']
+print(cities[2]) 
+print(cities[-2]) 
+
+languages = ['Python', 'Java', 'C++', 'Ruby']
+print(languages[3])  
+print(languages[0]) 
+
+
