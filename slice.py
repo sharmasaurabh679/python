b79a4bd5cc4d9f3e6bd04ba71794cebd09e6afbc
saurabh sharma <saurabhsharma.yash2004@gmail.com>
2025-09-08 21:14:41 +0530

    day 3

---
+++ slice.py
@@ -0,0 +1,20 @@
+s = 'thinktank'
+print(s[5:5])
+
+s = 'follow'
+print(s[3:8])
+
+s = 'medium'
+print(s[-4:4])
+
+s = 'coder'
+print(s[::1])
+
+s = 'doubled'
+print(s[1:6][1:3])
+
+
+s = 'completed'
+print(s[2:5:3])
+
+
